5a0fe25a6f8a254bb5355700206cc8e650751982
Mohammed AlZiyad <113702772+Mohammed-AlZiyad@users.noreply.github.com>
2023-01-31 10:04:02 +0300

    Add files via upload

---
+++ ApplePie/Game.swift
@@ -0,0 +1,30 @@
+//
+//  Game.swift
+//  Apple pie
+//
+//  Created by Mohammed AlZiyad on 04/07/1444 AH.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+        if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
